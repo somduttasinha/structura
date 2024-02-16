9698ae5392a55667212c29f7957e3aa21d1d1e8e
Som Sinha <somdsinha@gmail.com>
2024-02-16 21:21:45 +0100

    [new] equals and hashcode for Vertex

--- structura/src/main/java/com/somduttasinha/structura/graph/Vertex.java
+++ structura/src/main/java/com/somduttasinha/structura/graph/Vertex.java
@@ -1,8 +1,6 @@
 package com.somduttasinha.structura.graph;
 
-/**
- * Vertex in a Graph structure.
- */
+/** Vertex in a Graph structure. */
 public class Vertex<T> {
     private T data;
 
@@ -18,4 +16,33 @@ public class Vertex<T> {
         this.data = data;
     }
 
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((data == null) ? 0 : data.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        Vertex<?> other = (Vertex<?>) obj;
+        if (data == null) {
+            if (other.data != null) {
+                return false;
+            }
+        } else if (!data.equals(other.data)) {
+            return false;
+        }
+        return true;
+    }
 }
